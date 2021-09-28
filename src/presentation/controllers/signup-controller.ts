c001644a11651d2ee9ea3e9579f7e03cb37554ee
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-09-28 12:30:25 -0300

    feat: ensure SignUpController return 400 if no email provided

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -4,7 +4,7 @@ import { Controller, HttpRequest, HttpResponse } from '../protocols'
 
 class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name']
+    const requiredFields = ['name', 'email']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
